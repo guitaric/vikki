c9c3f79f05e648376e8b89b393a6c4c87e8e9698
taric-treat <taric@treat.agency>
2024-05-21 00:14:08 +0200

    video feature not a bug

--- dist/test.bfc5c598.js
+++ dist/test.bfc5c598.js
@@ -39520,7 +39520,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60978" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62733" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
